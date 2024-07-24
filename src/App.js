d1fbdbf90a0c03911c426317eb3618c9b20501b9
Shruthi Patel G M <patel.gm.shruthi@ibm.com>
2024-07-24 20:34:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
 
   return (
     // <ChatGpt />
-    // <ThreeCrossThreeWithStaticPattern />
-    <DynamicSize />
+    <ThreeCrossThreeWithStaticPattern />
+    // <DynamicSize />
   );
 }
 
